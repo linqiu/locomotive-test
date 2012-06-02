29d22655534835d740089fcce6502a1fa49394c9
Lin Qiu <linq@mac.com>
2012-06-02 01:36:24 -0400

    first commit with md doc

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LocomotiveTest::Application.config.secret_token = 'abc2327cdd93f65708e21066b5951586c3b6254ae48629ebd9942fee58463af2f1bd973597ce7cf75ba69049d049909087c5993f57419c297c5b1f43a34e3ef6'
